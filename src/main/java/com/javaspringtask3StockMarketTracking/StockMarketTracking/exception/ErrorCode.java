75c8ffe17de9a8a1d0609daa78500b9f9cd8f59c
yasinenessisik <128436451+yasinenessisik@users.noreply.github.com>
2024-04-11 19:32:30 +0300

    SocketModule güncellendi işlevselliği istendiği şekilde sağlandı. AckCallBack mekanizması, Pagination, eklenicek. Cache de eklenebilir.

--- src/main/java/com/javaspringtask3StockMarketTracking/StockMarketTracking/exception/ErrorCode.java
+++
@@ -1,6 +0,0 @@
-package com.javaspringtask3StockMarketTracking.StockMarketTracking.exception;
-
-public enum ErrorCode {
-    STOCK_NOT_FOUND,
-    ERROR_WHILE_UPDATING
-}
--- src/main/java/com/javaspringtask3StockMarketTracking/StockMarketTracking/repository/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javaspringtask3StockMarketTracking.StockMarketTracking.repository;
-
-import com.javaspringtask3StockMarketTracking.StockMarketTracking.model.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockRepository extends JpaRepository<Stock,Integer> {
-}
